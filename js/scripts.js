21f16e3d614cb3b0e2d609a7f76142634e1d6e3b
Brooke Perry <perrysbrooke@gmail.com>
2018-10-23 22:06:00 -0700

    add function to output array as string

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,42 @@
+function vowelsToDashes(array) {
+  var newArray = [];
+  array.forEach(function(letter){
+    if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
+      newArray.push("-");
+    } else {
+      newArray.push(letter);
+    }
+  })
+  return newArray;
+};
+
+
+
+
+
+
 $(document).ready(function() {
   $("#forLoop").submit(function(event) {
     event.preventDefault();
 
     var inputNumber1 = parseInt($("#inputNum1").val());
     var inputNumber2 = parseInt($("#inputNum2").val());
-console.log(inputNumber1);
+
     for (var index = 0; index <= inputNumber2; index += inputNumber1) {
       alert(index)
     }
+  });
+
+  $("form.sentence").submit(function(event) {
+    event.preventDefault();
+
+    var sentence1 = ($("#sentenceInput").val()).split("");
+    console.log(sentence1);
+    var sentenceWithVowelsRemoved = vowelsToDashes(sentence1).join("");
+    console.log(sentenceWithVowelsRemoved);
+
+    $("#sentenceOutput").text(sentenceWithVowelsRemoved);
+
 
   });
 });
